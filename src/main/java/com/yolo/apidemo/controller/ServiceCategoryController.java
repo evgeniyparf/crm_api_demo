52eca7f6570e640d6078adc4ad254bceb173da7e
evgeniyparf <evgeniy.parf@gmail.com>
2018-10-25 19:09:18 +0300

    Added ServiceCategoryService

--- src/main/java/com/yolo/apidemo/controller/ServiceCategoryController.java
+++ src/main/java/com/yolo/apidemo/controller/ServiceCategoryController.java
@@ -1,12 +1,9 @@
 package com.yolo.apidemo.controller;
 
 import com.yolo.apidemo.model.ServiceCategory;
-import com.yolo.apidemo.repository.ServiceCategoryRepository;
+import com.yolo.apidemo.service.ServiceCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -15,44 +12,34 @@ import java.util.List;
 @RestController
 //@RequestMapping("/api")
 public class ServiceCategoryController {
+
     @Autowired
-    private ServiceCategoryRepository serviceCategoryRepository;
+    private ServiceCategoryService serviceCategoryService;
 
     @GetMapping("/service_categories")
-    public List<ServiceCategory> getAllServiceCategories(@RequestParam(name = "page", required = false, defaultValue = "0") Integer page,
-                                                         @RequestParam(name = "size", required = false, defaultValue = "20") Integer size,
-                                                         @RequestParam(name = "name", required = false) String name){
-        if(name != null)
-            return serviceCategoryRepository.findByTitleIgnoreCaseContaining(name);
-        else
-            return serviceCategoryRepository.findAll(PageRequest.of(page, size)).getContent();
+    public List<ServiceCategory> getAllServiceCategories(@RequestParam(name = "page", required = false, defaultValue = "0") int page,
+                                                         @RequestParam(name = "size", required = false, defaultValue = "20") int size,
+                                                         @RequestParam(name = "title", required = false) String title){
+        return serviceCategoryService.getAllServiceCategories(page, size, title);
     }
 
     @GetMapping("/service_categories/{id}")
     public ServiceCategory getServiceCategory(@PathVariable int id){
-        return serviceCategoryRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("ServiceBuilder Category " + id + " not found"));
+        return serviceCategoryService.getServiceCategory(id);
     }
 
     @PutMapping("/service_categories/{id}")
-    public HttpStatus updateServiceCategory(@PathVariable int id, @Valid @RequestBody ServiceCategory serviceCategoryDetails){
-        ServiceCategory serviceCategory = serviceCategoryRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Category " + id + " not found"));
-        if(serviceCategoryDetails.getTitle() != null)
-            serviceCategory.setTitle(serviceCategoryDetails.getTitle());
-        serviceCategoryRepository.save(serviceCategory);
-        return HttpStatus.OK;
+    public ServiceCategory updateServiceCategory(@PathVariable int id, @Valid @RequestBody ServiceCategory serviceCategoryDetails){
+        return serviceCategoryService.updateServiceCategory(id, serviceCategoryDetails);
     }
 
     @PostMapping("/service_categories")
     public ServiceCategory newServiceCategory(@Valid @RequestBody ServiceCategory serviceCategory){
-        return serviceCategoryRepository.save(serviceCategory);
+        return serviceCategoryService.newServiceCategory(serviceCategory);
     }
 
     @DeleteMapping("/service_categories/{id}")
-    public ResponseEntity<?> deleteServiceCategory(@PathVariable int id){
-        serviceCategoryRepository.delete(serviceCategoryRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("ServiceBuilder Category " + id + "not found")));
-        return ResponseEntity.ok().build();
+    public HttpStatus deleteServiceCategory(@PathVariable int id){
+        return serviceCategoryService.deleteServiceCategory(id);
     }
 }
---
+++ src/main/java/com/yolo/apidemo/service/ServiceCategoryService.java
@@ -0,0 +1,47 @@
+package com.yolo.apidemo.service;
+
+import com.yolo.apidemo.model.ServiceCategory;
+import com.yolo.apidemo.repository.ServiceCategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ServiceCategoryService {
+    @Autowired
+    private ServiceCategoryRepository serviceCategoryRepository;
+
+    public List<ServiceCategory> getAllServiceCategories(int page, int size, String title){
+        if(title != null)
+            return serviceCategoryRepository.findByTitleIgnoreCaseContaining(title);
+        else
+            return serviceCategoryRepository.findAll(PageRequest.of(page, size)).getContent();
+    }
+
+    public ServiceCategory getServiceCategory(int id){
+        return serviceCategoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("ServiceBuilder Category " + id + " not found"));
+    }
+
+    public ServiceCategory updateServiceCategory(int id, ServiceCategory serviceCategoryDetails){
+        ServiceCategory serviceCategory = serviceCategoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category " + id + " not found"));
+        if(serviceCategoryDetails.getTitle() != null)
+            serviceCategory.setTitle(serviceCategoryDetails.getTitle());
+        serviceCategoryRepository.save(serviceCategory);
+        return serviceCategory;
+    }
+
+    public ServiceCategory newServiceCategory(ServiceCategory serviceCategory){
+        return serviceCategoryRepository.save(serviceCategory);
+    }
+
+    public HttpStatus deleteServiceCategory(int id){
+        serviceCategoryRepository.deleteById(id);
+        return HttpStatus.NO_CONTENT;
+    }
+}
